92b3f4d5f34f7b4836e9e668efe3f7346706e419
Daniel Seybold <daniel.seybold@uni-ulm.de>
2016-01-03 15:25:03 +0100

    inital commit

---
+++ src/main/java/de/uniulm/omi/ycsb/start/App.java
@@ -0,0 +1,12 @@
+package de.uniulm.omi.ycsb.start;
+
+/**
+ * Created by Daniel Seybold on 03.01.2016.
+ */
+public class App {
+
+    public static void main (String args[]){
+
+        System.out.println("hellow world");
+    }
+}
